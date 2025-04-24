47d351975b5900f553cff1383914c1bd89b811b2
Laura Anacleto <laur4kauany@gmail.com>
2025-04-24 14:16:53 -0300

    Arquivo novo sobre Exercicios Logica

--- Exercícicos de Lógica/lopal_ex071.ino
+++ Exercícicos de Lógica/lopal_ex071.ino
@@ -8,6 +8,7 @@ String cargo = "";
 float salarioInicial = 0;
 float salarioAcrescido = 0;
 
+
 void setup()
 {
   Serial.begin (9600);
@@ -43,9 +44,23 @@ void loop()
   
   //processamento
   salarioAcrescido = salarioInicial + (salarioInicial * 0.10);
-  
-  //saída
-  Serial.println("O seu nome eh: " + String (nome) );
-  Serial.println("O seu salario acrescido eh: " + String (salarioAcrescido) );
-  delay(5000);//aguarda 5 segundos antes de rodar o loop novamente
+
+   
+  //Saída
+  Serial.println();
+  Serial.println();
+  Serial.println("Relatorio:");
+  Serial.println();
+  Serial.println("-------------------------------");
+  Serial.println();
+
+
+  Serial.println("Seu nome: " + nome);
+  Serial.println("Seu cargo: " + cargo);
+  Serial.println("Seu salarioInicial: " + String(salarioInicial));
+  Serial.println("Seu salarioAcrescido: " + String(salarioAcrescido));
+  Serial.println("-------------------------------");
+
+  Serial.println();
+  delay (6000);
 }
