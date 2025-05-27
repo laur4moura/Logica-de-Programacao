2d3c9baac350b9624de025336379fea38e22a42c
Laura Anacleto <laur4kauany@gmail.com>
2025-05-27 08:46:59 -0300

    Arquivo novo de jogo de dado

--- Exercícicos de Lógica/jogo_de_dado_simplificado_com_motor1.ino
+++ Exercícicos de Lógica/jogo_de_dado_simplificado_com_motor1.ino
@@ -1,106 +1,78 @@
-// C++ code
-//VARIÁVEIS
-
-int ledVerde = 12;
-int ledVermelho = 8;
-int ledAzul1= 7;
-int ledAzul2 = 4;
-int ledAzul3 = 2;
-int acertos; 
 #include <Servo.h>
 Servo motor;
 
-//srtea um numero randômico/ale[atorio de 1 até 7 - 1, ou seja, 6 
+ int ledVerde = 12;
+ int ledVermelho = 8;
+ int ledAzul1 = 7;
+ int ledAzul2 = 4;
+ int ledAzul3 = 2;
+ int acertos = 0;
 
-void desenhaCabecalho()
-{
+
+void desenhaCabecalho() {
   Serial.println("*");
-  Serial.println("    PROGRAMA   ");
-  Serial.println("  JOGO DE DADOS ");
-  Serial.println("                 *");
-  Serial.println();
+  Serial.println("PROGRAMA: JOGO DE DADOS");
+  Serial.println("                          *");
   delay(500);
 }
 
-
-void setup()
-{
+void setup() {
   Serial.begin(9600);
-  randomSeed(analogRead(0));//inicia e normaliza a porta randômica para evitar o mesmo valor
+  randomSeed(analogRead(0)); // Evita sempre o mesmo número sorteado
+
   motor.attach(13);
-  motor.write(0);
+  motor.write(0); // Move o motor
+
   pinMode(ledVerde, OUTPUT);
   pinMode(ledVermelho, OUTPUT);
   pinMode(ledAzul1, OUTPUT);
-  pinMode(ledAzul2, OUTPUT);  
-  pinMode(ledAzul3, OUTPUT); 
+  pinMode(ledAzul2, OUTPUT);
+  pinMode(ledAzul3, OUTPUT);
 }
 
-void loop()
-{
+void loop() {
   int palpite;
-  int numeroSorteado = random (1,7);
+  int numeroSorteado = random(1, 7); // Sorteia um número entre 1 e 6
 
   desenhaCabecalho();
-  //solicitar o palpite do usuario - 3 comandos
+  Serial.println("Digite o seu palpite (entre 1 e 6):");
 
-  Serial.println("Digite o seu palpite entre 1 a 6");
-  while(! Serial.available());  
+  while (!Serial.available());
   palpite = Serial.parseInt();
-  Serial.println();
-  Serial.println();
 
-  for(int i = 0; i <5; i ++){
-    Serial.print(".");
-
-    delay(500);
-  }
-
+  Serial.println("Processando...");
   delay(2000);
-  Serial.println();
-  Serial.println("O seu Palpite " + String(palpite));
-  Serial.println("O Numero Sorteado eh  " + String(numeroSorteado));
 
-  if(palpite == numeroSorteado)
-  {
-    Serial.println("Parabens, voce acertou!! :)");
-    digitalWrite(ledVerde, HIGH);  
-    digitalWrite(ledAzul1, HIGH);
+  Serial.println("O seu palpite: " + String(palpite));
+  Serial.println("O numero sorteado: " + String(numeroSorteado));
+
+  // Lógica de jogo e acionamento dos LEDs
+  if (palpite == numeroSorteado) {
+    Serial.println("Parabens, voce acertou!");
+    digitalWrite(ledVerde, HIGH);
     delay(500);
     digitalWrite(ledVerde, LOW);
-  } else if (acertos == 0){
-    digitalWrite(ledAzul1, HIGH);
-  }
-  else if(acertos == 1) {
-    digitalWrite(ledAzul2, HIGH);
-
-  }else if (acertos == 2){
-    digitalWrite(ledAzul2, HIGH);
-    acertos ++;
-
-
-
-  } else if (acertos >= 3) {
-    Serial.println("Você conquistou a sabedoria! A porta se abre...");
-    motor.write(90);
-
-  }
-  else{
+    acertos++; // Incrementa a contagem de acertos
+  } else {
+    Serial.println("Que pena, tente outra vez.");
     digitalWrite(ledVermelho, HIGH);
     delay(1000);
     digitalWrite(ledVermelho, LOW);
-    Serial.println("Que pena, tente outra vez");
   }
 
+  // Controla LEDs com base nos acertos
+  if (acertos == 1) {
+    digitalWrite(ledAzul1, HIGH);
+  } else if (acertos == 2) {
+    digitalWrite(ledAzul2, HIGH);
+  } else if (acertos >= 3) {
+    Serial.println("Voce conquistou a sabedoria! A porta se abre...");
+    motor.write(90); // Move o motor
+    digitalWrite(ledAzul3, HIGH);
+  }
 
-
-
-
-  Serial.println("Reinicie o programa");
+  Serial.println("Reinicie o programa para jogar novamente.");
   Serial.println();
   Serial.println();
-
-
-
-
+  delay(3000);
 }
